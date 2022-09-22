adff6bdaba50f00a096c47b20c17dd0d8a4d9ecc
Svetlana <84134255+SvetlanaHoxha@users.noreply.github.com>
2022-09-21 19:24:43 -0500

    Add files via upload

---
+++ hello-rust/target/debug/deps/hello_rust-632dcce39b4256c5.d
@@ -0,0 +1,5 @@
+/home/svetlana/Playground/hello-rust/target/debug/deps/hello_rust-632dcce39b4256c5.rmeta: src/main.rs
+
+/home/svetlana/Playground/hello-rust/target/debug/deps/hello_rust-632dcce39b4256c5.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/svetlana/Playground/hello-rust/target/debug/hello-rust: /home/svetlana/Playground/hello-rust/src/main.rs
